14293669680267e37738505c43e14f49f26e6a10
German Bobr <german.bobr@redmondsoftware.com>
2016-07-02 03:30:49 -0300

    Atencion sin turno

--- js/suMedicoAppRecepcion.js
+++ js/suMedicoAppRecepcion.js
@@ -22,7 +22,7 @@ var app = angular.module('suMedicoApp',["ngRoute"])
                 templateUrl: "vistas/recepcion/recepcion.html"
             })
             .when("/pacientes/recepcion/detalle", {
-                controller: "altaController",
+                controller: "detalleController",
                 controllerAs: "vm",
                 templateUrl: "vistas/recepcion/detalle.html"
             })
@@ -31,6 +31,7 @@ var app = angular.module('suMedicoApp',["ngRoute"])
                 templateUrl: "vistas/turnos/reserva/normal.html"
             }).when("/sinturno",{
                 controllerAs: "vm",
+                controller: "sinturnoController",
                 templateUrl: "vistas/recepcion/sinturno.html"
         }   );
 
@@ -38,12 +39,15 @@ var app = angular.module('suMedicoApp',["ngRoute"])
     });
 
 app.factory("paciente",function(){
+    rst = function (){
+        this.cuil=111111111;
+        this.nombre="Alberto Gomez"
+    }
+
+    if(typeof this.nombre === "undefined") rst();
+
     return {
-        reset:
-            function (){
-                this.cuil=111111111;
-                this.nombre="Alberto Gomez"
-            }
+        reset: rst
     };
 });
 
@@ -69,4 +73,28 @@ app.controller('pinController',['$scope', '$location', 'paciente', function($sco
     $scope.consultorio = Math.round(Math.random()*100);
     $scope.profesional = "Juan Perez";
 
+}]);
+
+app.controller('detalleController',['$scope', '$location', 'paciente', function($scope,$location,paciente) {
+    $scope.paciente = paciente;
+    $scope.fecha = new Date();
+    $scope.pin = Math.round(Math.random()*10000);
+    $scope.consultorio = Math.round(Math.random()*100);
+    $scope.profesional = "Juan Perez";
+
+}]);
+
+app.controller('sinturnoController', ['$scope', '$location', 'paciente', function($scope,$location,paciente) {
+    $scope.paciente = paciente;
+    $scope.fecha = new Date();
+    $scope.pin = Math.round(Math.random()*10000);
+    $scope.consultorio = Math.round(Math.random()*100);
+    $scope.profesional = "Juan Perez";
+
+    paciente.reset();
+
+    $scope.update = function (paciente) {
+        $scope.paciente = angular.copy(paciente);
+        $location.url("pacientes/recepcion/detalle");
+    }
 }]);
